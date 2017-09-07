96a4280aa05edd0d2ad6681b64ec455f75f0ad70
Sabir <sabir@qburst.com>
2017-09-07 12:59:23 +0530

    added containers folder

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Main from './containers/main.jsx';
+
+ReactDom.render(
+		<Main/>,
+	document.getElementById('app'));
---
+++ app/containers/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export class Main extends React.Component {
+	
+}
